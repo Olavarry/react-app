5b98f90a4f30e7e6be8979905bcdf460c9dacda2
Luis Olavaria <kukiluis89@gmail.com>
2025-04-06 21:22:31 -0600

    rendering dynamic content

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,37 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  items = [];
+
+  const getMessage = () => (items.length === 0 ? <p>No items found</p> : null);
 
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
     </>
+
+    //another way to implent this could be:
+    // explanation: when using an expression like  'true && 1' or "true && 'Luis'"" the result will e the second value
+    //in above cases the result will be 1 and Luis, respectively
+    //If the expression is something like 'false && 1' or "false && 'Luis'" the reult will be false and if this is inside Jsx code
+    //nothing will be rendered, so it is better than returning null
+
+    // return (
+    //     <>
+    //       <h1>List</h1>
+    //       {items.length === 0 && <p>No items found</p>}
+    //       <ul className="list-group">
+    //         {items.map((item) => (
+    //           <li key={item}>{item}</li>
+    //         ))}
+    //       </ul>
+    //     </>
   );
 }
 
