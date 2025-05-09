a70f18dac4253232c0dae999e4ad4419ccc779f6
Luis Olavaria <kukiluis89@gmail.com>
2025-05-08 23:44:11 -0600

    browsers to use

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,20 +35,9 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    }
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -64,10 +53,7 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    
   ],
 
   /* Run your local dev server before starting the tests */
