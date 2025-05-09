3d1dc5ee95a5e0d2790906ba73d7ff7b96aadea8
Luis Olavaria <kukiluis89@gmail.com>
2025-05-08 23:23:03 -0600

    exclude Playwright tests from vitest

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/Playwright/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*']
   },
 })
